1587b14be406c474de1a662d1e2b174a6c07d7ef
UG <55311933+UUGTech@users.noreply.github.com>
2024-12-04 15:54:02 +0900

    Make Better-Escape Better

--- config/nvim/lua/plugins/better-escape.lua
+++ config/nvim/lua/plugins/better-escape.lua
@@ -2,6 +2,20 @@ return {
 	"max397574/better-escape.nvim",
 	lazy = false,
 	config = function()
-		require("better_escape").setup()
+		require("better_escape").setup({
+			timeout = vim.o.timeoutlen,
+			default_mappings = false,
+			mappings = {
+				i = {
+					j = {
+						j = "<Esc>",
+					},
+				},
+				c = {},
+				t = {},
+				v = {},
+				n = {},
+			},
+		})
 	end,
 }
