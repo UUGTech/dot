cebdf99a9d0052c926be18964e34c02fb3d152d4
UG <55311933+UUGTech@users.noreply.github.com>
2024-12-16 22:34:18 +0900

    Multi Select on Telescope [Version 2]

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -6,21 +6,78 @@ return {
 	config = function()
 		local gfh_actions = require("telescope").extensions.git_file_history.actions
 
-		local select_one_or_multi = function(prompt_bufnr)
+		local actions = require("telescope.actions")
+
+		local function multiopen(prompt_bufnr, method)
+			local edit_file_cmd_map = {
+				vertical = "vsplit",
+				horizontal = "split",
+				tab = "tabedit",
+				default = "edit",
+			}
+			local edit_buf_cmd_map = {
+				vertical = "vert sbuffer",
+				horizontal = "sbuffer",
+				tab = "tab sbuffer",
+				default = "buffer",
+			}
 			local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
-			local multi = picker:get_multi_selection()
-			if not vim.tbl_isempty(multi) then
-				require("telescope.actions").close(prompt_bufnr)
-				for _, j in pairs(multi) do
-					if j.path ~= nil then
-						vim.cmd(string.format("%s %s", "edit", j.path))
+			local multi_selection = picker:get_multi_selection()
+
+			if #multi_selection > 1 then
+				require("telescope.pickers").on_close_prompt(prompt_bufnr)
+				pcall(vim.api.nvim_set_current_win, picker.original_win_id)
+
+				for i, entry in ipairs(multi_selection) do
+					local filename, row, col
+
+					if entry.path or entry.filename then
+						filename = entry.path or entry.filename
+
+						row = entry.row or entry.lnum
+						col = vim.F.if_nil(entry.col, 1)
+					elseif not entry.bufnr then
+						local value = entry.value
+						if not value then
+							return
+						end
+
+						if type(value) == "table" then
+							value = entry.display
+						end
+
+						local sections = vim.split(value, ":")
+
+						filename = sections[1]
+						row = tonumber(sections[2])
+						col = tonumber(sections[3])
+					end
+
+					local entry_bufnr = entry.bufnr
+
+					if entry_bufnr then
+						if not vim.api.nvim_buf_get_option(entry_bufnr, "buflisted") then
+							vim.api.nvim_buf_set_option(entry_bufnr, "buflisted", true)
+						end
+						local command = i == 1 and "buffer" or edit_buf_cmd_map[method]
+						pcall(vim.cmd, string.format("%s %s", command, vim.api.nvim_buf_get_name(entry_bufnr)))
+					else
+						local command = i == 1 and "edit" or edit_file_cmd_map[method]
+						if vim.api.nvim_buf_get_name(0) ~= filename or command ~= "edit" then
+							filename =
+								require("plenary.path"):new(vim.fn.fnameescape(filename)):normalize(vim.loop.cwd())
+							pcall(vim.cmd, string.format("%s %s", command, filename))
+						end
+					end
+
+					if row and col then
+						pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
 					end
 				end
 			else
-				require("telescope.actions").select_default(prompt_bufnr)
+				actions["select_" .. method](prompt_bufnr)
 			end
 		end
-
 		require("telescope").setup({
 			pickers = {
 				colorscheme = {
@@ -32,13 +89,23 @@ return {
 					i = {
 						["<C-k>"] = require("telescope.actions").move_selection_previous,
 						["<C-j>"] = require("telescope.actions").move_selection_next,
-						["<CR>"] = select_one_or_multi,
+						["<CR>"] = function(prompt_bufnr)
+							multiopen(prompt_bufnr, "default")
+						end,
+						["<C-v>"] = function(prompt_bufnr)
+							multiopen(prompt_bufnr, "vertical")
+						end,
 					},
 					n = {
 						["<C-k>"] = require("telescope.actions").move_selection_previous,
 						["<C-j>"] = require("telescope.actions").move_selection_next,
 						["jj"] = require("telescope.actions").close,
-						["<CR>"] = select_one_or_multi,
+						["<CR>"] = function(prompt_bufnr)
+							multiopen(prompt_bufnr, "default")
+						end,
+						["<C-v>"] = function(prompt_bufnr)
+							multiopen(prompt_bufnr, "vertical")
+						end,
 					},
 				},
 			},
