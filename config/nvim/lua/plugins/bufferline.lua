2495cc4330b2af876e6c69c6b1f12490e0de5f15
UG <yuji.iguchi.ug@gmail.com>
2025-03-07 09:57:19 +0900

    why did I miss this!?

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -2,7 +2,7 @@ return {
 	"akinsho/bufferline.nvim",
 	version = "*",
 	dependencies = "nvim-tree/nvim-web-devicons",
-	event = "BufReadPre",
+	event = { "BufReadPre", "BufNewFile" },
 	config = function()
 		require("bufferline").setup({
 			highlights = require("catppuccin.groups.integrations.bufferline").get(),
