efa08ffe4205581eb24a484ae2540cd9bec55c9d
UG <yuji.iguchi.ug@gmail.com>
2025-03-08 13:46:30 +0900

    only when you need

--- config/nvim/lua/plugins/modicator.lua
+++ config/nvim/lua/plugins/modicator.lua
@@ -7,7 +7,7 @@ return {
 		vim.o.number = true
 		vim.o.termguicolors = true
 	end,
-	event = "VeryLazy",
+	event = { "BufReadPre", "BufNewFile" },
 	opts = {
 		-- Warn if any required option above is missing. May emit false positives
 		-- if some other plugin modifies them, which in that case you can just
