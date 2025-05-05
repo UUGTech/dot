35a3791e62ca7f7f4f3598b6e31ba4c70d3abf74
UG <yuji.iguchi.ug@gmail.com>
2025-05-05 11:17:03 +0900

    stiffness

--- config/nvim/lua/plugins/smear-cursor.lua
+++ config/nvim/lua/plugins/smear-cursor.lua
@@ -3,7 +3,7 @@ return {
 	event = "VeryLazy",
 	opts = { -- Default  Range
 		cursor_color = "#93a1a1",
-		stiffness = 0.7,
+		stiffness = 0.9,
 		trailing_stiffness = 0.6,
 		distance_stop_animating = 0.4,
 		legacy_computing_symbols_support = true,
