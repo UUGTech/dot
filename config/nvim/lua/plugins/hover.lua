f30a0f2acebbcfe1f84f04a83655956893360162
UG <55311933+UUGTech@users.noreply.github.com>
2025-05-31 12:25:42 +0900

    I don't need this

--- config/nvim/lua/plugins/hover.lua
+++ config/nvim/lua/plugins/hover.lua
@@ -39,12 +39,6 @@ return {
 			end
 		end, { desc = "hover.nvim" })
 		vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
-		vim.keymap.set("n", "<C-p>", function()
-			require("hover").hover_switch("previous")
-		end, { desc = "hover.nvim (previous source)" })
-		vim.keymap.set("n", "<C-n>", function()
-			require("hover").hover_switch("next")
-		end, { desc = "hover.nvim (next source)" })
 
 		-- Mouse support
 		vim.keymap.set("n", "<MouseMove>", require("hover").hover_mouse, { desc = "hover.nvim (mouse)" })
